9ca22dbfa4e1da610690d6262f7735efaa68a7e7
Usama Jamil <usama_csuet@hotmail.com>
2025-06-04 20:35:13 -0400

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Usama Jamil on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
